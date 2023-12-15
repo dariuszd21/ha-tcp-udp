4242cf2256ff0429a67c163ee1a96544853c2a9b
Darek D <darekduda21@gmail.com>
2023-12-15 14:00:05 +0100

    Add logger interface

---
+++ logger/logger.go
@@ -0,0 +1,51 @@
+package logger
+
+import (
+	"path/filepath"
+	"log"
+	"os"
+)
+
+type Logger struct {
+	erorLogger *log.Logger
+	debugLogger *log.Logger
+	printLogs bool
+}
+
+func (l* Logger) Debug(log_entry string) {
+	if l.printLogs {
+		log.Print(log_entry)
+	}
+	l.debugLogger.Print(log_entry)
+}
+
+func (l* Logger) error(log_entry string) {
+	if l.printLogs {
+		log.Print(log_entry)
+	}
+	l.erorLogger.Print(log_entry)
+}
+
+func openLogFile(path string, flags int, mode int) *os.File {
+	logfile, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatalf("Cannot open error %s.", path)
+	}
+	return logfile
+}
+
+func NewLogger(printLogs bool) *Logger {
+	// TODO: Read from configuration file
+	logPath := os.TempDir()
+	flags := os.O_APPEND|os.O_CREATE|os.O_WRONLY 
+	mode := 0600
+	error_logfile := openLogFile(filepath.Join(logPath, "error.log"), flags, mode)
+	debug_logfile := openLogFile(filepath.Join(logPath, "debug.log"), flags, mode)
+
+	return &Logger{
+		erorLogger: log.New(error_logfile, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile),
+		debugLogger: log.New(debug_logfile, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile),
+	}
+}
+
+
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	// "ha-tcp-udp/config"
+	"ha-tcp-udp/logger"
+	// "ha-tcp-udp/tcp_server"
+)
+
+func main() {
+	logger_obj := logger.NewLogger(true) 	
+
+	logger_obj.Debug("Some debug print")
+}

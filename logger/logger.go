9c66cc343a4e328e8dda2c10dab26e37ff0ba9db
Darek D <darekduda21@gmail.com>
2023-12-15 14:25:48 +0100

    Add log level handling

--- logger/logger.go
+++ logger/logger.go
@@ -1,31 +1,50 @@
 package logger
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 )
 
+const NONE = 0
+const ERROR = 1
+const DEBUG = 2
+
 type Logger struct {
 	erorLogger  *log.Logger
 	debugLogger *log.Logger
 	printLogs   bool
+	logLevel    int
 }
 
 func (l *Logger) Debug(log_entry string) {
-	l.printLogsToConsole(log_entry)
-	l.debugLogger.Print(log_entry)
+	if l.logLevel >= DEBUG {
+		l.printLogsToConsole(log_entry)
+		l.debugLogger.Print(log_entry)
+	}
 }
 
 func (l *Logger) Error(log_entry string) {
-	l.printLogsToConsole(log_entry)
-	l.erorLogger.Print(log_entry)
+	if l.logLevel >= ERROR {
+		l.printLogsToConsole(log_entry)
+		l.erorLogger.Print(log_entry)
+	}
 }
 
 func (l *Logger) printLogsToConsole(log_entry string) {
 	if l.printLogs {
 		log.Print(log_entry)
 	}
+}
+
+func (l *Logger) setLogLevel(level int) {
+	if (level < NONE) || (level > DEBUG) {
+		l.Error(fmt.Sprintf("Cannot set level to %d", level))
+		return
+	}
+
+	l.logLevel = level
 }
 
 func openLogFile(path string, flags int, mode int) *os.File {
@@ -59,4 +78,8 @@ func Debug(log_entry string) {
 
 func Error(log_entry string) {
 	LOGGER.Error(log_entry)
+}
+
+func SetLogLevel(log_level int) {
+	LOGGER.setLogLevel(log_level)
 }
--- main.go
+++ main.go
@@ -9,4 +9,6 @@ import (
 func main() {
 
 	logger.Debug("Some debug print")
+	logger.SetLogLevel(logger.DEBUG)
+	logger.Debug("Some debug print")
 }

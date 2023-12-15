34487302359fca7501f2ebe7b4c6c94306c64b6b
Darek D <darekduda21@gmail.com>
2023-12-15 18:22:06 +0100

    Add running server via interface

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"ha-tcp-udp/tcp_server"
 )
 
+func runServer(server server_if.Server) {
+	server.Bind()
+	server.Serve()
+}
+
 func main() {
 
 	logger.SetLogLevel(logger.DEBUG)
@@ -17,6 +22,5 @@ func main() {
 		Port: 12000,
 	}
 	server := tcp_server.CreateServer(&server_config)
-	server.Bind()
-	server.Serve()
+	runServer(server)
 }
